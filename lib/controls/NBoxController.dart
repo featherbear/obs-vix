75d3dc3fb8bd2f198528a92e414c032c061fff92
Andrew Wong <andrew.j.wong@outlook.com>
2021-09-01 03:27:53 +1000

    Formatting

--- lib/controls/NBoxController.dart
+++ lib/controls/NBoxController.dart
@@ -35,57 +35,66 @@ class _NBoxControllerState extends State<NBoxController> {
 
     Widget generateRow(int idx) {
       var name = "$NBOX_SWITCHER_PREFIX$idx";
-      List<String>? source = sources[name];
-      if (source == null) return Text("Missing nbox scene: $name");
+
+      Widget wrap() {
+        List<String>? source = sources[name];
+        if (source == null)
+          return Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Missing nbox scene: $name",
+                style: TextStyle(color: Colors.red),
+              ));
+
+        return source.isNotEmpty
+            ? RadioButtonRow(
+                buttons: source,
+                activeColour: COLOUR.YELLOW,
+                activeLabel: activeNBoxSource[name],
+                onButtonPress: (idx) {
+                  String activeItem = source[idx];
+                  setState(() {
+                    activeNBoxSource[name] = activeItem;
+                  });
+                  for (String sourceName in source) {
+                    this.client.request(
+                        command: "SetSceneItemRender", params: {"scene-name": name, "source": sourceName, "render": sourceName == activeItem});
+                    // this.client.request(
+                    //     command: "SetSceneItemProperties",
+                    //     params: {"scene-name": source.key, "item": sourceName, "visible": sourceName == activeItem});
+                  }
+                },
+              )
+            : Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "No items in this switcher",
+                  style: TextStyle(color: Colors.grey),
+                ));
+      }
 
       return Container(
-          color: Colors.green,
           child: Padding(
               padding: EdgeInsets.only(bottom: 15),
               child: Column(
-                // crossAxisAlignment: CrossAxisAlignment.start,
+                  // crossAxisAlignment: CrossAxisAlignment.start,
 
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    child: Padding(
-                        padding: EdgeInsets.only(bottom: 5),
-                        child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              "${VIXUtils.processLabel(name)}",
-                              style: TextStyle(fontSize: 24),
-                            ))),
-                  ),
-                  source.isNotEmpty
-                      ? RadioButtonRow(
-                          buttons: source,
-                          activeColour: COLOUR.YELLOW,
-                          activeLabel: activeNBoxSource[name],
-                          onButtonPress: (idx) {
-                            String activeItem = source[idx];
-                            setState(() {
-                              activeNBoxSource[name] = activeItem;
-                            });
-                            for (String sourceName in source) {
-                              this.client.request(
-                                  command: "SetSceneItemRender",
-                                  params: {"scene-name": name, "source": sourceName, "render": sourceName == activeItem});
-                              // this.client.request(
-                              //     command: "SetSceneItemProperties",
-                              //     params: {"scene-name": source.key, "item": sourceName, "visible": sourceName == activeItem});
-                            }
-                          },
-                        )
-                      : Align(alignment: Alignment.centerLeft, child: Text("No items in this switcher"))
-                ],
-              )));
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(bottom: 5),
+                      child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "${VIXUtils.processLabel(name)}",
+                            style: TextStyle(fontSize: 24),
+                          )),
+                    ),
+                    wrap()
+                  ])));
     }
 
     // Source transition?
 
-    return Container(
-        color: Colors.red,
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [for (int i = 1; i <= n_boxes; i++) generateRow(i)]));
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [for (int i = 1; i <= n_boxes; i++) generateRow(i)]);
   }
 }
