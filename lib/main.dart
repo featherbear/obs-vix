359d99d83578393ad4646db88ec92087c9077c48
Andrew Wong <andrew.j.wong@outlook.com>
2021-09-04 20:39:20 +1000

    Remove that boilerplate counter button :')

--- lib/main.dart
+++ lib/main.dart
@@ -23,21 +23,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vix :: OBS Vision Mixer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.grey,
       ),
       home: MyHomePage(title: 'Vix :: OBS Vision Mixer'),
@@ -48,15 +38,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -64,8 +45,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   _MyHomePageState() {
     SharedPreferences.getInstance().then((prefs) {
       {
@@ -168,12 +147,6 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   final VIXClient client = VIXClient(); //..addRawListener((data) => log(data));
 
   final focusNode = FocusNode()..requestFocus();
@@ -183,12 +156,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return RawKeyboardListener(
         focusNode: focusNode,
         onKey: (evt) {
@@ -211,108 +178,93 @@ class _MyHomePageState extends State<MyHomePage> {
           }
         },
         child: Scaffold(
-          appBar: AppBar(
-            title: Text(widget.title),
-            actions: [
-              PopupMenuButton(
-                  onSelected: (Function fn) => fn(),
-                  itemBuilder: (context) => [
-                        PopupMenuItem(
-                          child: Text("Connection Settings"),
-                          value: this.showConnectionSettingsPage,
-                        ),
-                        PopupMenuItem(
-                          child: Text("Interface Settings"),
-                          value: () => {
-                            Navigator.push(
-                                context,
-                                PageTransition(
-                                    type: PageTransitionType.fade,
-                                    child: PageViewWrapper(
-                                        title: "Vix Interface Settings",
-                                        child: Padding(
-                                            padding: EdgeInsets.symmetric(horizontal: 15),
-                                            child: provideVIXState(SettingsAssignmentView(
-                                              prefill: AssignmentSettings(buttons: readVIXState()["buttons"], nBoxes: readVIXState()["nBoxes"]),
-                                              saveCallback: (settings) {
-                                                SharedPreferences.getInstance().then((prefs) {
-                                                  prefs.setInt("vix::nBoxes", settings.nBoxes);
-                                                  prefs.setStringList("vix::buttons", settings.buttons.map((s) => s != null ? s : "").toList());
-                                                });
-                                                updateVIXState((m) {
-                                                  m["buttons"] = settings.buttons;
-                                                  m["nBoxes"] = settings.nBoxes;
-                                                });
-                                                if (settings.nBoxes > 0) {
-                                                  NBox_funcs.createNBoxes(this.client, n: settings.nBoxes).then((_) {
-                                                    NBox_funcs.getNBoxSources(this.client)
-                                                        .then((nBoxSources) => updateVIXState((m) => m["nBoxSources"] = nBoxSources));
+            appBar: AppBar(
+              title: Text(widget.title),
+              actions: [
+                PopupMenuButton(
+                    onSelected: (Function fn) => fn(),
+                    itemBuilder: (context) => [
+                          PopupMenuItem(
+                            child: Text("Connection Settings"),
+                            value: this.showConnectionSettingsPage,
+                          ),
+                          PopupMenuItem(
+                            child: Text("Interface Settings"),
+                            value: () => {
+                              Navigator.push(
+                                  context,
+                                  PageTransition(
+                                      type: PageTransitionType.fade,
+                                      child: PageViewWrapper(
+                                          title: "Vix Interface Settings",
+                                          child: Padding(
+                                              padding: EdgeInsets.symmetric(horizontal: 15),
+                                              child: provideVIXState(SettingsAssignmentView(
+                                                prefill: AssignmentSettings(buttons: readVIXState()["buttons"], nBoxes: readVIXState()["nBoxes"]),
+                                                saveCallback: (settings) {
+                                                  SharedPreferences.getInstance().then((prefs) {
+                                                    prefs.setInt("vix::nBoxes", settings.nBoxes);
+                                                    prefs.setStringList("vix::buttons", settings.buttons.map((s) => s != null ? s : "").toList());
                                                   });
-                                                }
-                                                ;
-                                                Navigator.pop(context);
-                                              },
-                                            ))))))
-                          },
-                        )
-                      ])
-            ],
-          ),
-          body: Center(
-            child: Padding(
+                                                  updateVIXState((m) {
+                                                    m["buttons"] = settings.buttons;
+                                                    m["nBoxes"] = settings.nBoxes;
+                                                  });
+                                                  if (settings.nBoxes > 0) {
+                                                    NBox_funcs.createNBoxes(this.client, n: settings.nBoxes).then((_) {
+                                                      NBox_funcs.getNBoxSources(this.client)
+                                                          .then((nBoxSources) => updateVIXState((m) => m["nBoxSources"] = nBoxSources));
+                                                    });
+                                                  }
+                                                  ;
+                                                  Navigator.pop(context);
+                                                },
+                                              ))))))
+                            },
+                          )
+                        ])
+              ],
+            ),
+            body: Center(
+              child: Padding(
                 padding: EdgeInsets.all(15),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                        padding: EdgeInsets.only(bottom: 15),
-                        child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, crossAxisAlignment: CrossAxisAlignment.center, children: [
-                          Container(
-                              // color: Colors.blue[200],
-                              child: Column(children: [
-                            SizedBox(width: MediaQuery.of(context).size.width * 0.40, child: previewSourceViewer),
-                            Padding(padding: EdgeInsets.symmetric(vertical: 5), child: Text("PREVIEW"))
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                          padding: EdgeInsets.only(bottom: 15),
+                          child: Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, crossAxisAlignment: CrossAxisAlignment.center, children: [
+                            Container(
+                                // color: Colors.blue[200],
+                                child: Column(children: [
+                              SizedBox(width: MediaQuery.of(context).size.width * 0.40, child: previewSourceViewer),
+                              Padding(padding: EdgeInsets.symmetric(vertical: 5), child: Text("PREVIEW"))
+                            ])),
+                            Container(
+                                // color: Colors.green[200],
+                                child: Column(children: [
+                              SizedBox(width: MediaQuery.of(context).size.width * 0.40, child: programSourceViewer),
+                              Padding(padding: EdgeInsets.symmetric(vertical: 5), child: Text("PROGRAM"))
+                            ]))
                           ])),
-                          Container(
-                              // color: Colors.green[200],
-                              child: Column(children: [
-                            SizedBox(width: MediaQuery.of(context).size.width * 0.40, child: programSourceViewer),
-                            Padding(padding: EdgeInsets.symmetric(vertical: 5), child: Text("PROGRAM"))
-                          ]))
-                        ])),
 
-                    // provideVIXState(ProgramView(this.client)),
-                    // buildVIXProvider((context, data) => SourceView(sourceName: data["activeProgram"])),
-                    provideVIXState(PreviewProgramController(
-                      onPreviewEvent: this.client.handleChangePreview,
-                      onProgramEvent: (idx) {
-                        String? targetScene = readVIXState()["buttons"][idx];
-                        if (targetScene == null) return;
-                        client.request(command: "SetCurrentScene", params: {"scene-name": targetScene});
-                      },
-                    )),
+                      // provideVIXState(ProgramView(this.client)),
+                      // buildVIXProvider((context, data) => SourceView(sourceName: data["activeProgram"])),
+                      provideVIXState(PreviewProgramController(
+                        onPreviewEvent: this.client.handleChangePreview,
+                        onProgramEvent: (idx) {
+                          String? targetScene = readVIXState()["buttons"][idx];
+                          if (targetScene == null) return;
+                          client.request(command: "SetCurrentScene", params: {"scene-name": targetScene});
+                        },
+                      )),
 
-                    Expanded(
-                      child: ListView(
-                        children: [
-                          provideVIXState(NBoxController(this.client)),
-                          Text(
-                            'You have pushed the button this many times:',
-                          ),
-                          Text(
-                            '$_counter',
-                            style: Theme.of(context).textTheme.headline4,
-                          ),
-                        ],
+                      Expanded(
+                        child: ListView(
+                          children: [provideVIXState(NBoxController(this.client))],
+                        ),
                       ),
-                    ),
-                  ],
-                )),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ), // This trailing comma makes auto-formatting nicer for build methods.
-        ));
+                    ],
+                  )),
+            )));
   }
 }
